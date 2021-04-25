8ed5955a8e66bb5f7db6208b1195dc0ac47d1cfb
Erick-Perez12 <82849144+Erick-Perez12@users.noreply.github.com>
2021-04-25 15:06:14 -0500

    Update problema7.cpp

--- problema7.cpp
+++ problema7.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 //problema 7
-int *prim;
 bool esPrimo(int numero) {
   if (numero == 0 || numero == 1 || numero == 4) return false;
   for (int x = 2; x < numero / 2; x++) {
@@ -10,34 +9,15 @@ bool esPrimo(int numero) {
   return true;
 }
 int main(){
-  /*int n;
-  prim=new int[n];
-  cout<<"Indicar en que posicion se encuentra el numero: ";
-  cin>>n;
-  for(int i=1;i<=n;i++){
-  	for(int j=1;j<15;j++){
-  		if(esPrimo(j)){
-  			prim[i]=j;
-  			i+=1;
-		  }
-	  }
-  }
-cout<<prim[n];*/
-int n,s;
-
-	for(int j=1;j<100000;j++){
-		n=0;
-		if(n!=6){
-			s=1;
-			if(esPrimo(j)){
-				s=s*j;
-				n++;
+int n=0;
+	for(int j=2;j<500000;j++){
+		if(esPrimo(j)){
+			n++;
+			if(n==10001){
+				cout<<"el numero: "<<j;
 			}
 		}
-		else{
-			cout<<s;
-		}
 	}
-
 return 0;
 }
+
